a0ebd2b51a2519d4a0c12fa770eabf6efbd6ece8
matheus <matheusdev20@gmail.com>
2025-04-11 21:42:36 -0300

    fix: timezone first render problem

--- app/components/app-calendar.tsx
+++ app/components/app-calendar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Calendar } from "@/components/ui/calendar";
-import { ptBR } from "date-fns/locale/pt-BR";
+// import { ptBR } from "date-fns/locale/pt-BR";
 
 type Props = {
   startNewDaily: (date: Date) => void;
@@ -13,9 +13,8 @@ export const AppCalendar = ({ startNewDaily }: Props) => {
   return (
     <div className="flex justify-center">
       <Calendar
-        locale={ptBR}
+        timeZone="-03:00"
         disabled={(date) => date > new Date()}
-        selected={new Date()}
         onDayClick={handleDateClick}
         // onDayClick={handleDateClick}
         className="rounded-md border"
