89e39b764fe55f47240cdc341d157252c530aba4
matheus <matheusdev20@gmail.com>
2025-04-13 16:31:59 -0300

    test: see if edge function is messing the vercel env

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
+// import { auth } from "./app/api/auth/auth";
 
-import { auth } from "@/src/app/api/auth/auth";
+// import { auth } from "@/src/app/api/auth/auth";
 
 const privateRoutes = ["/", "/team"];
 
 export async function middleware(request: NextRequest) {
-  const session = await auth();
-
+  // console.log("Run!");
+  // const session = await auth();
+  const session = null;
+  console.log("Session", session);
+  // const session = false;
   const { pathname } = request.nextUrl;
+
   const isProtectedRoute = privateRoutes.some((route) => route === pathname);
   if (!session && isProtectedRoute) {
+    console.log("Is protected route and no session", isProtectedRoute);
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
