499def2aba2622a2ace505702f4fa1128a29c1a0
matheus <matheusdev20@gmail.com>
2025-04-23 00:39:34 -0300

    trying prisma monorepo plugin

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { NextConfig } from "next";
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
   images: {
     domains: ["avatars.githubusercontent.com", "media.licdn.com"],
   },
