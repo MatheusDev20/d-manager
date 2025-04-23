1e09e5aa6048f3e44d0f0efea30e5550862416d1
matheus <matheusdev20@gmail.com>
2025-04-23 01:10:01 -0300

    new stuff

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { NextConfig } from "next";
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
   images: {
-    domains: ["avatars.githubusercontent.com", "media.licdn.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
