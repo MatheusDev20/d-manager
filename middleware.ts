10afdd83b8a53432adf4b36ad4bb3faedadbe8b7
matheus <matheusdev20@gmail.com>
2025-05-01 21:26:32 -0300

    removing possible source of load bug

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ interface SessionData {
   isLoggedIn: boolean;
 }
 
-const protectedRoutes = ["/", "/team", "/daily", "/settings", "/api"];
+const protectedRoutes = ["/", "/team", "/daily", "/settings"];
 const publicRoutes = ["/login", "/register", "/forgot-password"];
 
 const sessionOptions = {
@@ -36,10 +36,10 @@ export async function middleware(request: NextRequest) {
       );
 
       if (!session.isLoggedIn) {
-        const fromAPI = pathname.startsWith("/api");
-        if (fromAPI) {
-          return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-        }
+        // const fromAPI = pathname.startsWith("/api");
+        // if (fromAPI) {
+        //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        // }
         const url = new URL("/login", request.url);
         return NextResponse.redirect(url);
       }
@@ -55,9 +55,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/",
-    "/(.*)",
-    // But still exclude static assets and images
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
